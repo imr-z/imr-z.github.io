faea0faee173f524a15c6628d83584c4b7335bee
Mr.Z <152813401+imr-z@users.noreply.github.com>
2023-12-05 13:05:12 +0700

    Update index.js

--- scam/js/index.js
+++ scam/js/index.js
@@ -7,13 +7,11 @@ function itemScam (cname, csite, cdate, cuid) {
     var miFrame = document.getElementById("miframe");
     var th1 = document.getElementById("mname");
     var tp = document.getElementById("mdate");
-    var mrel = document.createElement("link");
+    var mrel = document.createElement("meta");
     if (site == cuid) {
-        mrel.setAttribute("rel", "icon");
-        mrel.setAttribute("type", "image/x-icon");
-        mrel.setAttribute("href", "csite");
+        mrel.setAttribute("property", "og:image");
+        mrel.setAttribute("content", csite);
         document.getElementById("main").style.display = "block";
-        miFrame.setAttribute("content", "frame-ancestors 'none'");
         miFrame.setAttribute("src", csite);
 
         th1.innerHTML = "<sup style='font-size:14px;'>By </sup>"+cname;

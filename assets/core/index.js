38e14dee6388be237c1bd54ec450257966de1ba5
Mr.Z <152813401+imr-z@users.noreply.github.com>
2023-12-06 17:51:42 +0700

    Update index.js

--- assets/core/index.js
+++ assets/core/index.js
@@ -1,12 +1,2 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDS5CtMFlNkE71okd_eajcJ6p9d2vY3lWQ",
-    authDomain: "i-mr-z.firebaseapp.com",
-    databaseURL: "https://i-mr-z-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "i-mr-z",
-    storageBucket: "i-mr-z.appspot.com",
-    messagingSenderId: "1085327472414",
-    appId: "1:1085327472414:web:431fc1455455076a33fb0f",
-    measurementId: "G-XVVBSGHX4H"
-};
-
-firebase.initializeApp(firebaseConfig);
+/* Powered by Mr.Z | 06 Dec 2023 */
+var firebaseConfig={apiKey:"AIzaSyDS5CtMFlNkE71okd_eajcJ6p9d2vY3lWQ",authDomain:"i-mr-z.firebaseapp.com",databaseURL:"https://i-mr-z-default-rtdb.asia-southeast1.firebasedatabase.app",projectId:"i-mr-z",storageBucket:"i-mr-z.appspot.com",messagingSenderId:"1085327472414",appId:"1:1085327472414:web:431fc1455455076a33fb0f",measurementId:"G-XVVBSGHX4H"};firebase.initializeApp(firebaseConfig);
